39855e458cd3760c117600cc42285ae8ae748ef0
wsilvadev <wsilvadev@gmail.com>
2024-03-19 20:52:23 -0300

    feat: create particles component and remove default components

---
+++ src/components/Particles.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="particles-js"></div>
+</template>
+
+<script>
+import particlesJSON from '@/utils/particle.json'
+export default {
+  name: 'ParticlesComponent',
+  mounted() {
+    // Load particles.js lib
+    const particlesScript = document.createElement('script')
+    particlesScript.src = 'http://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js'
+    document.head.appendChild(particlesScript)
+
+    // Initialize particles.js
+    particlesScript.onload = () => {
+      particlesJS('particles-js', particlesJSON)
+    }
+  }
+}
+</script>
+
+<style scoped>
+#particles-js {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  right: 0;
+  background-color: transparent;
+  overflow: hidden;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import Particles from '@/components/Particles.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <Particles />
   </main>
 </template>
